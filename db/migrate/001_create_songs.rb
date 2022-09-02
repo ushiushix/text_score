f610ef543ed1298f608c8f2c43f138f0b086968a
Koichi Inoue <ushiushix@gmail.com>
2022-09-03 00:50:26 +0900

    Fix migrations to work with activerecord 6

--- db/migrate/001_create_songs.rb
+++ db/migrate/001_create_songs.rb
@@ -1,4 +1,4 @@
-class CreateSongs < ActiveRecord::Migration
+class CreateSongs < ActiveRecord::Migration[4.2]
   def self.up
     create_table :songs do |t|
       t.string :title
--- db/migrate/005_change_accounts_sirname_to_description.rb
+++ db/migrate/005_change_accounts_sirname_to_description.rb
@@ -1,4 +1,4 @@
-class ChangeAccountsSirnameToDescription < ActiveRecord::Migration
+class ChangeAccountsSirnameToDescription < ActiveRecord::Migration[4.2]
   def change
     rename_column :accounts, :surname, :description
   end
